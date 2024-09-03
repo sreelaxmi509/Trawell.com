66dbd68564308a9fdf89df4899f3aef0a7f105e4
sreelaxmi509 <136480763+sreelaxmi509@users.noreply.github.com>
2024-09-03 12:38:50 +0530

    Add files via upload

---
+++ trawell.com/profile.js
@@ -0,0 +1,50 @@
+// Get necessary elements from the DOM
+const popupButton = document.getElementById('popupButton');
+const message = document.getElementById('message');
+const closeButton = document.getElementById('closeButton');
+popupButton.addEventListener('click', () => {
+  message.style.display = 'block';
+});
+closeButton.addEventListener('click', () => {
+  message.style.display = 'none';
+});
+//place2
+const popupButton1 = document.getElementById('change-button1');
+const message1 = document.getElementById('message1');
+const closeButton1 = document.getElementById('closeButton1');
+popupButton1.addEventListener('click', () => {
+    message1.style.display = 'block';
+  });
+  closeButton1.addEventListener('click', () => {
+    message1.style.display = 'none';
+  });
+  //place3
+const popupButton2= document.getElementById('change-button2');
+const message2 = document.getElementById('message2');
+const closeButton2 = document.getElementById('closeButton2');
+popupButton2.addEventListener('click', () => {
+    message2.style.display = 'block';
+  });
+  closeButton2.addEventListener('click', () => {
+    message2.style.display = 'none';
+  });
+    //place4
+const popupButton3= document.getElementById('change-button3');
+const message3 = document.getElementById('message3');
+const closeButton3 = document.getElementById('closeButton3');
+popupButton3.addEventListener('click', () => {
+    message3.style.display = 'block';
+  });
+  closeButton3.addEventListener('click', () => {
+    message3.style.display = 'none';
+  });
+      //place5
+const popupButton4= document.getElementById('change-button4');
+const message4 = document.getElementById('message4');
+const closeButton4 = document.getElementById('closeButton4');
+popupButton4.addEventListener('click', () => {
+    message4.style.display = 'block';
+  });
+  closeButton4.addEventListener('click', () => {
+    message4.style.display = 'none';
+  });
